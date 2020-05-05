b73769afb9a276c1ae0dbb1b079b4bd2e0558a0f
Daniel Becker <db@fino.digital>
2020-05-05 11:30:59 +0200

    Make the image version variable

--- docker/build.sh
+++ docker/build.sh
@@ -2,7 +2,8 @@
 DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd -P)"
 
 IMAGE_NAME=nexus-oss/nexus3
-IMAGE_VERSION=3.19.1-01
+IMAGE_VERSION=3.22.1
 
 docker build -t ${IMAGE_NAME}:${IMAGE_VERSION} \
+             --build-arg NEXUS_VERSION=${IMAGE_VERSION} \
              -f "${DIR}/Dockerfile" "${DIR}"
